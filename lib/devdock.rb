9218d9532aa63456d3b3f3a7e34d9a9c9958d340
Júnior <b.sep@live.com>
2025-03-01 00:13:26 -0300

    remove docker check

--- lib/devdock.rb
+++ lib/devdock.rb
@@ -6,22 +6,7 @@ require_relative 'devdock/version'
 module Devdock
   class << self
     def create(name)
-      display_install_dependencies and return unless docker_installed?
-
       Setup.new(name).build
-    end
-
-    private
-
-    def docker_installed?
-      !`docker -v 2> /dev/null`.empty?
-    end
-
-    def display_install_dependencies
-      puts "You need to install docker to use devdock.\n" \
-           'To information about how install it, visit: https://docs.docker.com/engine/install/'
-
-      true
     end
   end
 end
--- test/test_devdocker.rb
+++ test/test_devdocker.rb
@@ -16,31 +16,16 @@ class TestDevdock < Minitest::Test
   end
 
   def test_create_project
-    Devdock.stub(:docker_installed?, true) do
-      assert_output(
-        "      create  example\n      create  example/Dockerfile.dev\n      create  example/docker-compose.yml\n" \
-        "      create  example/Makefile\n"
-      ) do
-        Devdock.create(@name)
-      end
-
-      assert(Dir.exist?(@name))
-      assert_includes(Dir.entries(@name), 'Dockerfile.dev')
-      assert_includes(Dir.entries(@name), 'docker-compose.yml')
-      assert_includes(Dir.entries(@name), 'Makefile')
+    assert_output(
+      "      create  example\n      create  example/Dockerfile.dev\n      create  example/docker-compose.yml\n" \
+      "      create  example/Makefile\n"
+    ) do
+      Devdock.create(@name)
     end
-  end
 
-  def test_display_message_if_docker_isnt_present
-    Devdock.stub(:docker_installed?, false) do
-      assert_output(
-        "You need to install docker to use devdock.\n" \
-        "To information about how install it, visit: https://docs.docker.com/engine/install/\n"
-      ) do
-        Devdock.create(@name)
-      end
-
-      refute(Dir.exist?(@name))
-    end
+    assert(Dir.exist?(@name))
+    assert_includes(Dir.entries(@name), 'Dockerfile.dev')
+    assert_includes(Dir.entries(@name), 'docker-compose.yml')
+    assert_includes(Dir.entries(@name), 'Makefile')
   end
 end
