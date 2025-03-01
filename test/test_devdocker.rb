31a6b57b96817b51ced0ddb6a07e3f6e7e8b2b3a
Júnior <b.sep@live.com>
2025-02-28 23:53:56 -0300

    display message if docker isnt installed

--- test/test_devdocker.rb
+++ test/test_devdocker.rb
@@ -5,15 +5,10 @@ require 'test_helper'
 class TestDevdocker < Minitest::Test
   def setup
     @name = 'example'
-    @original_stdout = $stdout
-
-    $stdout = StringIO.new
   end
 
   def teardown
     FileUtils.rm_rf([@name])
-  ensure
-    $stdout = @original_stdout
   end
 
   def test_that_it_has_a_version_number
@@ -21,11 +16,31 @@ class TestDevdocker < Minitest::Test
   end
 
   def test_create_project
-    Devdocker.create(@name)
+    Devdocker.stub(:docker_installed?, true) do
+      assert_output(
+        "      create  example\n      create  example/Dockerfile.dev\n      create  example/docker-compose.yml\n" \
+        "      create  example/Makefile\n"
+      ) do
+        Devdocker.create(@name)
+      end
 
-    assert(Dir.exist?(@name))
-    assert_includes(Dir.entries(@name), 'Dockerfile.dev')
-    assert_includes(Dir.entries(@name), 'docker-compose.yml')
-    assert_includes(Dir.entries(@name), 'Makefile')
+      assert(Dir.exist?(@name))
+      assert_includes(Dir.entries(@name), 'Dockerfile.dev')
+      assert_includes(Dir.entries(@name), 'docker-compose.yml')
+      assert_includes(Dir.entries(@name), 'Makefile')
+    end
+  end
+
+  def test_display_message_if_docker_isnt_present
+    Devdocker.stub(:docker_installed?, false) do
+      assert_output(
+        "You need to install docker to use devduck.\n" \
+        "To information about how install it, visit: https://docs.docker.com/engine/install/\n"
+      ) do
+        Devdocker.create(@name)
+      end
+
+      refute(Dir.exist?(@name))
+    end
   end
 end
