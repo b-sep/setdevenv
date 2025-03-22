b31235e9f9d62371f44a793b2b256352fe5a4836
Júnior <b.sep@live.com>
2025-03-22 00:25:14 -0300

    remove unnecessary tests since the only way to create the project is through the cli

--- lib/set_dev_env.rb
+++ lib/set_dev_env.rb
@@ -4,7 +4,4 @@ require_relative 'set_dev_env/setup'
 require_relative 'set_dev_env/version'
 
 module SetDevEnv
-  def self.create(name)
-    Setup.new(name).build
-  end
 end
--- test/test_set_dev_env.rb
+++ test/test_set_dev_env.rb
@@ -3,29 +3,7 @@
 require 'test_helper'
 
 class TestSetDevEnv < Minitest::Test
-  def setup
-    @name = 'example'
-  end
-
-  def teardown
-    FileUtils.rm_rf([@name])
-  end
-
   def test_that_it_has_a_version_number
     refute_nil ::SetDevEnv::VERSION
-  end
-
-  def test_create_project
-    assert_output(
-      "      create  example\n      create  example/Dockerfile.dev\n      create  example/docker-compose.yml\n" \
-      "      create  example/Makefile\n"
-    ) do
-      SetDevEnv.create(@name)
-    end
-
-    assert(Dir.exist?(@name))
-    assert_includes(Dir.entries(@name), 'Dockerfile.dev')
-    assert_includes(Dir.entries(@name), 'docker-compose.yml')
-    assert_includes(Dir.entries(@name), 'Makefile')
   end
 end
